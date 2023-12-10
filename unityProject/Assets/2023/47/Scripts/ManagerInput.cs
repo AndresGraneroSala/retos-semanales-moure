ade895ce020f666bfa52bab00c2967709380bfad
AndresGS <andresnn21@gmail.com>
2023-12-10 20:38:09 +0100

    works points in debug

--- unityProject/Assets/2023/47/Scripts/ManagerInput.cs
+++ unityProject/Assets/2023/47/Scripts/ManagerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,46 @@ using UnityEngine.UI;
 public class ManagerInput : MonoBehaviour
 {
     [SerializeField] private InputField input;
+    private const  int numA32bit= 97;
+    private const  int numÑAlphabet= 15;
+    private const int totalLettersInAlphabet=27;
+    
     //private char [] letters=
     public void SubmitWord()
     {
-        print(input.text);
 
         string word = input.text;
-        
+
+        char[] letters = word.ToCharArray();
+
+        int points=0;
+
+        foreach (var letter in letters)
+        {
+            if (char.ToLower(letter) == 'ñ')
+            {
+                points += numÑAlphabet;
+                print("ñ");
+                continue;
+            }
+
+            int letterPos32bit = Convert.ToInt32(char.ToLower(letter));
+
+            bool isUpperÑ = false;
+
+            if (letterPos32bit >= numA32bit + numÑAlphabet - 1)
+            {
+                isUpperÑ = true;
+                print("ñ " + letter);
+            }
+
+
+
+            points += char.ToLower(letter) - (numA32bit - 1);
+            points += isUpperÑ ? 1 : 0;
+        }
+
+        print(points);
         input.text = "";
         
         
