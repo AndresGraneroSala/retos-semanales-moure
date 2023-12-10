923e60c46ba7dda12bfb4ed8d2a2a07747c2117c
AndresGS <andresnn21@gmail.com>
2023-12-10 22:01:58 +0100

    build web

--- unityProject/Assets/2023/47/Scripts/ManagerInput.cs
+++ unityProject/Assets/2023/47/Scripts/ManagerInput.cs
@@ -3,54 +3,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Text;
+using System;
+using System.Globalization;
+using System.Text;
 public class ManagerInput : MonoBehaviour
 {
     [SerializeField] private InputField input;
     private const  int numA32bit= 97;
     private const  int numÑAlphabet= 15;
     private const int totalLettersInAlphabet=27;
+
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform content;
     
-    //private char [] letters=
     public void SubmitWord()
     {
 
         string word = input.text;
 
-        char[] letters = word.ToCharArray();
+        char[] letters = word.Replace(" ","").ToCharArray();
 
-        int points=0;
+        int points = 0;
 
         foreach (var letter in letters)
         {
-            if (char.ToLower(letter) == 'ñ')
-            {
-                points += numÑAlphabet;
-                print("ñ");
-                continue;
-            }
-
-            int letterPos32bit = Convert.ToInt32(char.ToLower(letter));
-
-            bool isUpperÑ = false;
-
-            if (letterPos32bit >= numA32bit + numÑAlphabet - 1)
-            {
-                isUpperÑ = true;
-                print("ñ " + letter);
-            }
-
-
-
-            points += char.ToLower(letter) - (numA32bit - 1);
-            points += isUpperÑ ? 1 : 0;
+            points += GetPointsLetter(letter);
         }
+        
+        GameObject textPoints = Instantiate(prefab,content);
+        textPoints.GetComponent<Text>().text = $"Points: {points} - Word: {word}";
 
-        print(points);
+        if (points==100)
+        {
+            textPoints.GetComponent<Text>().color= Color.yellow;
+        }
+        
+        
         input.text = "";
+
+    }
+
+    int GetPointsLetter(char letter)
+    {
+        int points = 0;
+
+        char letterClear = letter.ToString().Normalize(NormalizationForm.FormD).ToLower()[0];
         
-        
-        
+        if (char.ToLower(letterClear) == 'ñ'){return numÑAlphabet;}
+
+        int letterPos32bit = Convert.ToInt32(char.ToLower(letter));
+
+        bool isUpperÑ = letterPos32bit >= numA32bit + numÑAlphabet - 1;
+
+        points += char.ToLower(letter) - (numA32bit - 1);
+        points += isUpperÑ ? 1 : 0;
+        return points;
     }
 
     public void GoBackEdit()
